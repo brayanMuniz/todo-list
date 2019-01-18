98dccb07c626a6bd5ec386e9c7307406181fa29f
Brayan Muniz <brayanmuniz2636@gmail.com>
2019-01-18 09:24:24 -0600

    small quick fix

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var express = require('express')
 var app = express()
 const port = 6969;
 var hbs = require('express-hbs');
-var path = require('path');
-let firebase = require('./firebase')
+let firebase = require('./firebase');
+
 // What Express is going to uses
 app.use(express.static('public'))
-// Use `.hbs` for extensions and find partials in `views/partials`.
 app.engine('hbs', hbs.express4({
     partialsDir: __dirname + '/views/partials'
 }));
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
@@ -17,13 +17,15 @@ app.set('views', __dirname + '/views');
 app.get('/', (req, res) => {
 
     firebase.readRoot().then(fres => {
-        fres.forEach(doc => {
-            console.log("​doc", doc.data())
+        // fres.forEach(doc => {
+        //     console.log("​doc", doc.data())
+        // })
 
-        })
+        console.log(fres);
         res.render('landing.hbs', {
-            rootData: doc.data()
+            rootData: 'hello'
         })
+
     }).catch(err => {
         console.log(err)
     })
