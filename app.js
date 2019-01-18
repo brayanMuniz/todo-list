08ad6da3936cb546d4a507b4f86f3bd15c238813
Brayan Muniz <35124364+brayanMuniz@user.noreply.github.com>
2019-01-18 10:50:45 -0600

    moved to test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser');
 const port = 6969;
 var hbs = require('express-hbs');
 let firebase = require('./firebase');
@@ -7,17 +8,15 @@ let firebase = require('./firebase');
 // What Express is going to uses
 // app.use(express.static('public'))
 app.use(express.static(__dirname + '/public'));
-
+app.use(bodyParser.json()); // for parsing application/json
 app.engine('hbs', hbs.express4({
     partialsDir: __dirname + '/views/partials'
 }));
-
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
 // Routes
 app.get('/', (req, res) => {
-
     firebase.readRoot().then(fres => {
         let totalData = []
         fres.forEach(el => {
@@ -31,6 +30,10 @@ app.get('/', (req, res) => {
     }).catch(err => {
         console.log(err)
     })
+})
+
+app.post('/', (req, res) => {
+    res.send(req)
 })
 
 app.listen(
